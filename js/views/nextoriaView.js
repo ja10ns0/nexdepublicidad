0feff63b9f65d4aee4e489adca997c3e9c8c99f0
ja10ns0 <alonsofernandezjaime@gmail.com>
2017-11-04 07:41:51 +0100

    nextoria modal development

--- js/views/nextoriaView.js
+++ js/views/nextoriaView.js
@@ -29,16 +29,36 @@ Nexapp.NextoriaView = Backbone.View.extend({
         // current type
         var currentType = this.$el.find('.current');
         // hide current type
-        $('.row.' + currentType.data('tipo')).hide();
+        $('#' + currentType.data('tipo')).hide();
         // toogle current type class
         currentType.toggleClass('current');
         $(e.currentTarget).toggleClass('current');
         // show clicked type 
-        $('.row.' + $(e.target).data('tipo')).show();
+        $('#' + $(e.target).data('tipo')).show();
     },
 
     openModal: function (e) {
-        $('#myModal').modal();
+        // clicked type
+        var cType = e.currentTarget.parentElement.id;
+        // clicked magazine
+        var cMag = e.target.id;
+        // insert content
+        $('.modal-title').html();
+        var src = NEXTORIA_TYPES[cType][cMag]['src'];
+        $('.nextoriaModal_img img').attr('src',src);
+        // open modal
+        $('#nextoriaModal').modal();
     }
 
 });  
+
+var NEXTORIA_TYPES = {
+    paisajismo : {
+        paisea : {
+            title: 'paisea',
+            edition: 'paisajismo, landscape and architecture',
+            leadership: 'Jos&eacute; Manuel Vidal',
+            src:'/assets/img/paisajismo/GRID_PAISEA.jpg'
+        }
+    }
+}
