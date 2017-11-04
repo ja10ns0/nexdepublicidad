4506516c6a0ff11168fab93863693fe32c5a6bff
ja10ns0 <alonsofernandezjaime@gmail.com>
2017-11-04 21:07:00 +0100

    nextoria development - slide

--- js/views/nextoriaView.js
+++ js/views/nextoriaView.js
@@ -6,6 +6,7 @@ var Nexapp = Nexapp || {};
 Nexapp.NextoriaView = Backbone.View.extend({
 
     controller: null,
+    currentMagazine: null,
     id: 'nextoria-section',
     renderCallback: null,
 
@@ -13,11 +14,16 @@ Nexapp.NextoriaView = Backbone.View.extend({
 
     events: {
         'click li': 'changeGrid',
-        'click .row:not(.subheader) div[class^=col-]': 'openModal'
+        'click .row:not(.subheader) div[class^=col-]': 'openModal',
+        'click .modal-footer span': 'slideMag',
     },
 
     initialize: function (options) {
         this.controller = options.controler;
+        this.currentMagazine = {
+            type: 'arquitectura',
+            index: undefined
+        }
         this.render();
     },
 
@@ -28,6 +34,10 @@ Nexapp.NextoriaView = Backbone.View.extend({
     changeGrid: function (e) {
         // current type
         var currentType = this.$el.find('.current');
+        this.currentMagazine = {
+            type : $(e.target).data('tipo'),
+            index: undefined
+        };
         // hide current type
         $('#' + currentType.data('tipo')).hide();
         // toogle current type class
@@ -42,182 +52,201 @@ Nexapp.NextoriaView = Backbone.View.extend({
         var cType = e.currentTarget.parentElement.id;
         // clicked magazine
         var cMag = e.target.id;
+        // update currentMagazine object
+        this.currentMagazine.index = cMag;
         // magazine object
         var magazine = NEXTORIA_TYPES[cType][cMag]; 
         // insert content
-        $('.modal-title').html();
+        this._insertContent(magazine);
+        // open modal
+        $('#nextoriaModal').modal();
+    },
+
+    slideMag: function(e) {
+        var direction = +e.currentTarget.id ? 1 : -1;
+        var cType = this.currentMagazine.type;
+        var cMag = +this.currentMagazine.index + direction;
+        this.currentMagazine.index = cMag;
+        // magazine object
+        var magazine = NEXTORIA_TYPES[cType][cMag];
+        // insert content
+        this._insertContent(magazine);
+        // check index
+        var firstMag = _.first(_.keys(NEXTORIA_TYPES[cType]));
+        var lastMag = _.last(_.keys(NEXTORIA_TYPES[cType]));
+        if(cMag == firstMag) {
+            $('span[id=0]').css('color','red');
+        }
+        if(cMag == lastMag) {
+            $('span[id=1]').css('color','red');
+        }
+    },
+
+    _insertContent: function(magazine) {
         $('.nextoriaModal_img img').attr('src',magazine.src);
         $('.nextoriaModal_info .title_text').html(magazine.title);
         $('.nextoriaModal_info .edition_text').html(magazine.edition);
         $('.nextoriaModal_info .leadership_text').html(magazine.leadership);
-        // open modal
-        $('#nextoriaModal').modal();
     }
 
 });  
 
 var NEXTORIA_TYPES = {
     arquitectura : {
-        a1 : {
+        1 : {
             title: 'Arquitectura',
             edition: 'Colegio Oficial de Arquitectos de Madrid COAM',
             leadership: 'Jer&oacute;nimo Junquera / Estanislao P&eacute;rez Pita',
-            src:'/assets/img/GRID_COAM_1.jpg'
+            src: '/assets/img/GRID_COAM_1.jpg'
         },
-        a2 : {
+        2 : {
             title: 'Arquitectura',
             edition: 'Colegio Oficial de Arquitectos de Madrid COAM',
             leadership: 'Sara de la Mata / Fuensanta Nieto / Enrique Sobejano',
-            src:'/assets/img/GRID_COAM_2.jpg'
+            src: '/assets/img/GRID_COAM_2.jpg'
         },
-        a3 : {
+        3 : {
             title: 'Arquitectura',
             edition: 'Colegio Oficial de Arquitectos de Madrid COAM',
             leadership: 'Fernando Porras Isla / Federico Soriano',
-            src:'/assets/img/GRID_COAM_3.jpg'
+            src: '/assets/img/GRID_COAM_3.jpg'
         },
-        a4 : {
+        4 : {
             title: 'Urbanismo COAM',
             edition: 'Colegio Oficial de Arquitectos de Madrid',
             leadership: 'Abel Anguita / Fernando Nasarre / Luis Rodr&iacute;guez Avial',
-            src:'/assets/img/arquitectura/GRID_COAM_URBANISMO.jpg'
+            src: '/assets/img/arquitectura/GRID_COAM_URBANISMO.jpg'
         },
-        a5 : {
+        5 : {
             title: 'Quaderns d\'arquitectura i urbanisme',
             edition: 'Colegio Oficial de Arquitectos de Catalu&ntildea COAC',
             leadership: 'Josep Lu&iacute;s Mateo',
-            src:'/assets/img/arquitectura/GRID_QUADERNS_1.jpg'
+            src: '/assets/img/arquitectura/GRID_QUADERNS_1.jpg'
         },
-        a6 : {
+        6 : {
             title: 'Quaderns d\'arquitectura i urbanisme',
             edition: 'Colegio Oficial de Arquitectos de Catalu&ntildea COAC',
             leadership: 'Josep Lu&iacute;s Mateo',
-            src:'/assets/img/arquitectura/GRID_QUADERNS_2.jpg'
+            src: '/assets/img/arquitectura/GRID_QUADERNS_2.jpg'
         },
-        a7 : {
+        7 : {
             title: 'Quaderns d\'arquitectura i urbanisme',
             edition: 'Colegio Oficial de Arquitectos de Catalu&ntildea COAC',
             leadership: 'Manel Gausa',
-            src:'/assets/img/arquitectura/GRID_QUADERNS_3.jpg'
+            src: '/assets/img/arquitectura/GRID_QUADERNS_3.jpg'
         },
-        a8 : {
+        8 : {
             title: 'Quaderns d\'arquitectura i urbanisme',
             edition: 'Colegio Oficial de Arquitectos de Catalu&ntildea COAC',
             leadership: 'Manel Gausa',
-            src:'/assets/img/arquitectura/GRID_QUADERNS_4.jpg'
+            src: '/assets/img/arquitectura/GRID_QUADERNS_4.jpg'
         },
-        a9 : {
-            title: '',
-            edition: '',
-            leadership: '',
-            src:'/assets/img/arquitectura/GRID_CSAE_1.jpg'
+        9 : {
+            title: 'Arquitectos',
+            edition: 'Consejo Superior de Arquitectos de España C.S.A.E',
+            leadership: 'Atxu Amann / Andr&eacute;s Canov&aacute;s',
+            src: '/assets/img/arquitectura/GRID_CSAE_1.jpg'
         },
-        arquitectos2 : {
-            title: '',
-            edition: '',
-            leadership: '',
-            src:''
+        10 : {
+            title: 'Arquitectos',
+            edition: 'Consejo Superior de Arquitectos de España C.S.A.E',
+            leadership: 'Ignacio Borego / N&eacute;stor Montenegro / Lina Toro',
+            src: '/assets/img/arquitectura/GRID_CSAE_2.jpg'
         },
-        ta : {
-            title: '',
-            edition: '',
-            leadership: '',
-            src:''
+        11 : {
+            title: 'Tecnolog&iacute;a y Arquitectura',
+            edition: 'Dirección de Arquitectura, Departamento de Urbanismo, Vivienda y Medio Ambiente del Gobierno Vasco',
+            leadership: 'Javier Mozas / Aurora Fern&aacute;ndez',
+            src: '/assets/img/arquitectura/GRID_Tecnologia y Arquitectura.jpg'
         },
-        amast : {
-            title: '',
-            edition: '',
-            leadership: '',
-            src:''
+        12 : {
+            title: 'A+T',
+            edition: 'Arquitectura y Tecnolog&iacute;a',
+            leadership: 'Javier Mozas / Aurora Fern&aacute;ndez',
+            src: '/assets/img/arquitectura/GRID_A+T.jpg'
         },
-        composicionarq : {
-            title: '',
-            edition: '',
-            leadership: '',
-            src:''
+        13 : {
+            title: 'Composici&oacute;n arquitect&oacute;nica',
+            edition: 'Fundaci&oacute;n Faustino Orbegozo',
+            leadership: 'Manuel &Iacute;&ntilde;iguez / Alberto Ust&aacute;rroz',
+            src: '/assets/img/arquitectura/GRID_COMPOSICION_ARQUITECTONICA.jpg'
         },
-        arssacra : {
-            title: '',
-            edition: '',
-            leadership: '',
-            src:''
+        14 : {
+            title: 'Ars Sacra',
+            edition: 'Patrimonio Cultural, Archivos, Artes Pl&aacute;sticas, Arquitectura y M&uacut;sica',
+            leadership: '&Aacute;ngel Sancho',
+            src: '/assets/img/arquitectura/GRID_ARS_SACRA.jpg'
         },
-        arssacra : {
-            title: '',
-            edition: '',
-            leadership: '',
-            src:''
+        15 : {
+            title: 'Basa',
+            edition: 'Colegio Oficial de Arquitectos de las Islas Canarias',
+            leadership: 'Federico Garc&iacutea; Barba',
+            src: '/assets/img/arquitectura/GRID_BASA.jpg'
         },
-        basa : {
-            title: '',
-            edition: '',
-            leadership: '',
-            src:''
+        16 : {
+            title: 'BAU',
+            edition: 'Colegio Oficial de Arquitectos de Castilla y Le&oacute;n y Colegio Oficial de Arquitectos de Castilla La Mancha',
+            leadership: 'Dar&iacute; &Aacute;lvarex / Alberto Combarros / Fernando de Castro',
+            src: '/assets/img/arquitectura/GRID_BAU.jpg'
         },
-        bau : {
-            title: '',
-            edition: '',
-            leadership: '',
-            src:''
+        17 : {
+            title: 'Geometr&iacute;',
+            edition: 'Revista Semestral de Arquitectura y Urbanismo',
+            leadership: 'Jos&eacute; Segu&iacute;',
+            src: '/assets/img/arquitectura/GRID_GEOMETRIA.jpg'
         },
-        geometria : {
-            title: '',
-            edition: '',
-            leadership: '',
-            src:''
+        18 : {
+            title: 'Loggia',
+            edition: 'Arquitectura y Restauraci&oacute;n',
+            leadership: 'Juan Francisco Noguera',
+            src: '/assets/img/arquitectura/GRID_LOGIA.jpg'
         },
-        loggia : {
-            title: '',
-            edition: '',
-            leadership: '',
-            src:''
+        19 : {
+            title: 'Periferia',
+            edition: 'Colegio Oficial de Arquitectos de Andaluc&iacute;',
+            leadership: 'Luis Ib&aacute;&ntilde;ez S&aacute;nchez / Antonio Jim&eacute;nez Torrecillas / Elisenda Monz&oacute;n / Mart&iacute;n Ram&iacute;rez P&eacute;rez',
+            src: '/assets/img/arquitectura/GRID_PERIFERIA.jpg'
         },
-        periferia : {
-            title: '',
-            edition: '',
-            leadership: '',
-            src:''
-        },
-        proun : {
-            title: '',
-            edition: '',
-            leadership: '',
-            src:''
-        },
+        20 : {
+            title: 'PROUN',
+            edition: 'Proyectos Fin de Carrera',
+            leadership: 'Juan Manuel Ros / Jos&eacute; Ram&oacute;n Salgado',
+            src: '/assets/img/arquitectura/GRID_PROUN.jpg'
+        }
     },
 
     anuarios : {
-        a2000 : {
-            title: '',
-            edition: '',
-            leadership: '',
-            src:''
+        21: {
+            title: 'Anuario 2000',
+            edition: 'Colegio Superior de los Colegios de Arquitectos de Espa&ntilde;a C.S.C.A.E',
+            leadership: '-',
+            src:'/assets/img/anuarios/FULL_GRID_CSAE_ANUARIO_2.jpg'
         },
-        a2005 : {
-            title: '',
-            edition: '',
-            leadership: '',
-            src:''
+        22 : {
+            title: 'Anuario 2005',
+            edition: 'Colegio Superior de los Colegios de Arquitectos de Espa&ntilde;a C.S.C.A.E',
+            leadership: '-',
+            src:'/assets/img/anuarios/FULL_GRID_CSAE_ANUARIO_1.jpg'
         }
     },
 
     construccion : {
-        cau : {
-            title: '',
-            edition: '',
-            leadership: '',
-            src:''
+        23 : {
+            title: 'CAU',
+            edition: 'Colegio Oficial de Aparejadores y Arquitectos T&eacute;cnicos de Barcelona',
+            leadership: 'Jaume Rosell',
+            src: '/assets/img/construccion/GRID_CAU.jpg'
         },
-        cercha : {
-            title: '',
-            edition: '',
-            leadership: '',
-            src:''
+        24 : {
+            title: 'CERCHA',
+            edition: 'Consejo General de Aparejadores y Arquitectos T&eacute;cnicos de Espa&ntilde;a',
+            leadership: '-',
+            src: '/assets/img/construccion/GRID_CERCHA.jpg'
         }
     },
     
     paisajismo : {
-        paisea : {
+        25 : {
             title: 'paisea',
             edition: 'paisajismo, landscape and architecture',
             leadership: 'Jos&eacute; Manuel Vidal',
@@ -226,71 +255,71 @@ var NEXTORIA_TYPES = {
     },
 
     diseno : {
-        dediseno :{
-            title: '',
-            edition: '',
-            leadership: '',
-            src:''
+        26 :{
+            title: 'DE DISEÑO',
+            edition: 'Editorial El Croquis',
+            leadership: 'Juli Capella / Quim Larrea / Pierluigi Cattermole',
+            src:'/assets/img/diseño/GRID_ARDI.jpg'
         },
-        ardi : {
-            title: '',
-            edition: '',
-            leadership: '',
-            src:''
+        27 : {
+            title: 'Ardi',
+            edition: 'Grupo Zeta',
+            leadership: 'Juli Capella / Quim Larrea',
+            src:'/assets/img/diseño/GRID_DEDISENO.jpg'
         }
     },
 
     internacional : {
-        compasses : {
-            title: '',
-            edition: '',
-            leadership: '',
-            src:''
+        28 : {
+            title: 'Compasses',
+            edition: '-',
+            leadership: 'Massimo de Falco / Marco Ferreti / Francesca Maderna',
+            src:'/assets/img/internacional/GRID_COMPASSES.jpg'
         },
-        build : {
-            title: '',
-            edition: '',
-            leadership: '',
-            src:''
+        29 : {
+            title: 'Build das architekten magazin',
+            edition: '-',
+            leadership: 'Johannes Busmann',
+            src:'/assets/img/internacional/GRID_BUILD.jpg'
         }
     },
 
-    cultural : {
-        deusto : {
-            title: '',
-            edition: '',
-            leadership: '',
-            src:''
+    culturales : {
+        30 : {
+            title: 'Deusto Business Review',
+            edition: 'Ediciones Deusto',
+            leadership: '-',
+            src:'/assets/img/culturales/GRID_DEUSTO.jpg'
         },
-        impar : {
-            title: '',
-            edition: '',
-            leadership: '',
-            src:''
+        31 : {
+            title: 'IMPAR',
+            edition: '-',
+            leadership: 'Mar&iacute;a Casanovas / Asunci&oacute;n Batlle / Luis Do&ntilde;ate',
+            src:'/assets/img/culturales/GRID_IMPAR.jpg'
         },
-        liquiddocs : {
-            title: '',
-            edition: '',
-            leadership: '',
-            src:''
+        32 : {
+            title: 'LIQUID',
+            edition: 'Art Live Art International Book Magazine',
+            leadership: 'Xavier Rovira',
+            src:'/assets/img/culturales/GRID_LIQUIDOCS.jpg'
         },
-        nhhoteles : {
-            title: '',
-            edition: '',
-            leadership: '',
-            src:''
+        33 : {
+            title: 'NH Hoteles',
+            edition: '-',
+            leadership: 'Javier Angulo',
+            src:'/assets/img/culturales/GRID_NH.jpg'
         },
-        madridstyle : {
-            title: '',
-            edition: '',
-            leadership: '',
-            src:''
+        34 : {
+            title: 'MADRID STYLE',
+            edition: 'BOOKSTYLE S.A',
+            leadership: '-',
+            src:'/assets/img/culturales/GRID_LIFESTYLE_M.jpg'
         },
-        barcelonastyle : {
-            title: '',
-            edition: '',
+        35 : {
+            title: 'BARCELONA STYLE',
+            edition: 'BOOKSTYLE S.A',
             leadership: '',
-            src:''
+            src:'/assets/img/culturales/GRID_LIFESTYLE_B.jpg'
         },
     },
 }
