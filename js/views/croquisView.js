7c4a92f805fd15ea5bcffc9bc392a2e977767c3b
ja10ns0 <alonsofernandezjaime@gmail.com>
2017-12-08 09:16:30 +0100

    Croquis page
    Import new photos

--- js/views/croquisView.js
+++ js/views/croquisView.js
@@ -5,40 +5,43 @@ var Nexapp = Nexapp || {};
 
 Nexapp.CroquisView = Backbone.View.extend({
 
-    controller: null,
-    id: 'croquis-section',
-    renderCallback : null,
-    
-    template : Nexapp.getTemplate('croquisView'),
+	controller: null,
+	id: 'croquis-section',
+	renderCallback: null,
 
-    initialize : function(options) {
-	this.controller = options.controler;
-	this.renderCallback = this._afterRender;
-	this.render();
-    },
+	template: Nexapp.getTemplate('croquisView'),
 
-    render : function() {
-	this.$el.html(this.template());
-    },
-    
-    _afterRender : function() {
-	var img1 = 'assets/img/ctabr2015.png';
-	var img2 = 'assets/img/ctabr2016.png';
-	var img3 = 'assets/img/ctdic2015.png';
-	var img4 = 'assets/img/ctfeb2016.png';
+	initialize: function (options) {
+		this.controller = options.controler;
+		this.renderCallback = this._afterRender;
+		this.render();
+	},
 
-	var images = [img1,img2,img3,img4];
-	
-	var fade = function(img){
-	    $("#fadein img")
-	    .fadeOut(2000, function() {$("#fadein img").attr('src',img);})
-	    .fadeIn(2000);
+	render: function () {
+		this.$el.html(this.template());
+	},
+
+	_afterRender: function () {
+
+		var img1 = 'assets/img/189.jpg';
+		var img2 = 'assets/img/190.jpg';
+		var img3 = 'assets/img/191.jpg';
+		var img4 = 'assets/img/192.jpg';
+
+		var images = [img1, img2, img3, img4];
+
+		var fade = function (img) {
+			$("#fadein img")
+			.fadeOut(2000, function () { $("#fadein img").attr('src', img); })
+			.fadeIn(2000);
+		}
+		setInterval(function () {
+			for (var i = 0; i < images.length; i++) { 
+				fade(images[i]); 
+			}
+		}, 4000);
+
+
 	}
-	setInterval(function(){ 
-	    for(var i=1; i<images.length;i++){fade(images[i]);}
-	}, 4100);
-
-	    
-    }
 
 });
