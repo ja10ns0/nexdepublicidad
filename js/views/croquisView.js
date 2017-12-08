4002f669ca86d521b6d2de7d4dce1cbde8962b83
ja10ns0 <alonsofernandezjaime@gmail.com>
2017-12-08 21:18:54 +0100

    Croquis
    Animate images function implementation without setInterval function.

--- js/views/croquisView.js
+++ js/views/croquisView.js
@@ -23,8 +23,6 @@ Nexapp.CroquisView = Backbone.View.extend({
 
 	_afterRender: function () {
 
-		var self = this;
-
 		var img1 = 'assets/img/189.jpg';
 		var img2 = 'assets/img/190.jpg';
 		var img3 = 'assets/img/191.jpg';
@@ -32,25 +30,18 @@ Nexapp.CroquisView = Backbone.View.extend({
 
 		var images = [img1, img2, img3, img4];
 
-		var fade = function (img) {
-			$("#fadein img")
-				.fadeOut(2000, function () { $("#fadein img").attr('src', img); })
-				.fadeIn(2000);
-		}
-		this.timer = setInterval(function () {
-			initValue = self.initialized ? 0 : 1;
-			var t0 = performance.now();
+		(function animateImages(isFirstInvocation) {
+			var initValue = isFirstInvocation ? 1 : 0;
+			var fade = function (img) {
+				$("#fadein img")
+					.fadeOut(3500, function () { $("#fadein img").attr('src', img); })
+					.fadeIn(3500);
+
+			};
 			for (var i = initValue; i < images.length; i++) {
-				self.initialized = true;
 				fade(images[i]);
 			}
-			var t1 = performance.now();
-			console.log('tiempo: ' + (t1 - t0));
-		}, 2000, self);
-	},
-
-	remove: function () {
-		Backbone.View.prototype.remove.call(this);
-		clearInterval(this.timer);
+			animateImages();
+		})(true);
 	}
 });
